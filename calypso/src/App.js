337a42c50a00074ec4a3cd248d6282cad89d9fec
Stephanie Benitez <stephybenitez82@gmail.com>
2021-07-29 23:30:24 -0500

    pruebas

--- calypso/src/App.js
+++ calypso/src/App.js
@@ -1,22 +1,14 @@
-import Logo-calypso from './logo-calypso.jpeg';
+import Logo from './Logo-calypso.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo-calypso} className="App-logo" alt="logo" />
+        <img src={Logo} className="App-logo" alt="logo" />
         <p>
           El mejor <code>lugar </code> para tus recetas.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
