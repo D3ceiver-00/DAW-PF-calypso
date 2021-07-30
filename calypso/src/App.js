edf68a2e52ff12267777738a067dba6ffc87a221
Stephanie Benitez <stephybenitez82@gmail.com>
2021-07-30 03:21:42 -0500

    pruebas

--- calypso/src/App.js
+++ calypso/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react'
+import { AuthContext } from './auth/AuthContext'
+import { authReducer } from './auth/authReducer';
+import { AppRouter } from './routers/AppRouter'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const init = () => {
+    // si existen un item en local storage con nombre de user, se regresa ese objeto
+    //   sino, se regresa un objeto con logged en falso.
+    return JSON.parse(localStorage.getItem('user')) || {logged:false};
 }
 
-export default App;
+export const HeroesApp = () => {
+    // con este hook de useReducer, se seguira el estado del usuario
+    //  usando el authReducer
+    const [user, dispatch] = useReducer(authReducer, {}, init);
+
+    useEffect(() => {
+        localStorage.setItem('user', JSON.stringify(user));
+    }, [user])
+
+    return (
+        <AuthContext.Provider value={{user, dispatch}}>
+            <AppRouter />
+        </AuthContext.Provider>
+    )
+}
+
